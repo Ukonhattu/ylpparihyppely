13dd4138250b7d64528c4e317218f3174b015ce8
Koch Daniel W <daxda@da3-cs-dk110-07.helsinki.fi>
2016-09-12 15:34:41 +0300

    ui rakennus alkoi

---
+++ ylpparihyppely/src/main/java/ylpparihyppely/ui/Application.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ylpparihyppely.ui;
+
+/**
+ *
+ * @author daxda
+ */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class Application extends JFrame {
+    
+    public Application() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(250, 200);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Application ex = new Application();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
