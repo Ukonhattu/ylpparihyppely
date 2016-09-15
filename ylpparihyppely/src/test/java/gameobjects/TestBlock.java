8b24c36d931b904dcf7f7c2cab66d9c146097634
Koch Daniel W <daxda@da3-cs-dk110-10.helsinki.fi>
2016-09-15 13:26:36 +0300

    juu

---
+++ ylpparihyppely/src/test/java/gameobjects/TestBlock.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameobjects;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ylpparihyppely.gameobjects.Block;
+import ylpparihyppely.gameobjects.BoxHitboxMaker;
+import ylpparihyppely.gameobjects.Location;
+
+/**
+ *
+ * @author daxda
+ */
+public class TestBlock {
+
+    Block block;
+    BoxHitboxMaker bmkr;
+
+    public TestBlock() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        this.block = new Block(new Location(30, 30), 40, 40);
+        this.bmkr = new BoxHitboxMaker(block, 40, 40);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConstructorHeight() {
+        assertEquals(true, this.block.getHeigth() == 40);
+    }
+
+    @Test
+    public void testConstructorWidth() {
+        assertEquals(true, this.block.getWidth() == 40);
+    }
+
+    @Test
+    public void testConstructorLocation() {
+        assertEquals(true, this.block.getLocation().equals(new Location(30, 30)));
+    }
+
+    @Test
+    public void testGetHitbox() {
+        assertEquals(true, this.bmkr.makeHitbox().equals(this.block.getHitbox()));
+    }
+}
